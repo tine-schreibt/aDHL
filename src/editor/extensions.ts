1212044ae3291b525c7e7acec0a9fa631de158aa
Miezepuh <173158012+tine-schreibt@users.noreply.github.com>
2025-01-18 12:56:57 +0100

    Dependency bullshit

--- src/editor/extensions.ts
+++ src/editor/extensions.ts
@@ -1,58 +1,68 @@
-import { defaultKeymap, history, historyKeymap, indentWithTab } from "@codemirror/commands";
+import {
+	defaultKeymap,
+	history,
+	historyKeymap,
+	indentWithTab,
+} from "@codemirror/commands";
 import { css } from "@codemirror/lang-css";
 import {
-  bracketMatching,
-  defaultHighlightStyle,
-  foldGutter,
-  foldKeymap,
-  indentOnInput,
-  syntaxHighlighting,
+	bracketMatching,
+	defaultHighlightStyle,
+	foldGutter,
+	foldKeymap,
+	indentOnInput,
+	syntaxHighlighting,
 } from "@codemirror/language";
 import { EditorState, Extension } from "@codemirror/state";
 import {
-  drawSelection,
-  dropCursor,
-  EditorView,
-  //highlightActiveLine,
-  //highlightActiveLineGutter,
-  highlightSpecialChars,
-  keymap,
-  lineNumbers,
-  rectangularSelection,
+	drawSelection,
+	dropCursor,
+	EditorView,
+	//highlightActiveLine,
+	//highlightActiveLineGutter,
+	highlightSpecialChars,
+	keymap,
+	lineNumbers,
+	rectangularSelection,
 } from "@codemirror/view";
 
-import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete";
+import {
+	autocompletion,
+	closeBrackets,
+	closeBracketsKeymap,
+	completionKeymap,
+} from "@codemirror/autocomplete";
 import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
 
 import { lintKeymap } from "@codemirror/lint";
 
 export const basicSetup: Extension[] = [
-  lineNumbers(),
-  // highlightActiveLineGutter(),
-  highlightSpecialChars(),
-  history(),
-  css(),
-  foldGutter(),
-  drawSelection(),
-  dropCursor(),
-  EditorState.allowMultipleSelections.of(true),
-  indentOnInput(),
-  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-  EditorView.lineWrapping,
-  bracketMatching(),
-  closeBrackets(),
-  autocompletion(),
-  rectangularSelection(),
-  // highlightActiveLine(),
-  highlightSelectionMatches(),
-  keymap.of([
-    ...closeBracketsKeymap,
-    ...defaultKeymap,
-    ...searchKeymap,
-    ...historyKeymap,
-    indentWithTab,
-    ...foldKeymap,
-    ...completionKeymap,
-    ...lintKeymap,
-  ]),
-].filter(ext => ext);
+	lineNumbers(),
+	// highlightActiveLineGutter(),
+	highlightSpecialChars(),
+	history(),
+	css(),
+	foldGutter(),
+	drawSelection(),
+	dropCursor(),
+	EditorState.allowMultipleSelections.of(true),
+	indentOnInput(),
+	syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+	EditorView.lineWrapping,
+	bracketMatching(),
+	closeBrackets(),
+	autocompletion(),
+	rectangularSelection(),
+	// highlightActiveLine(),
+	highlightSelectionMatches(),
+	keymap.of([
+		...(closeBracketsKeymap as any[]),
+		...(defaultKeymap as any[]),
+		...(searchKeymap as any[]),
+		...(historyKeymap as any[]),
+		indentWithTab,
+		...(foldKeymap as any[]),
+		...(completionKeymap as any[]),
+		...(lintKeymap as any[]),
+	]),
+].filter((ext) => ext);
